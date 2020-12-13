40ed0e538a75b57705a041511fd01a3da215b042
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-12 22:48:23 -0600

    finished the byte short long exercise

--- Hello/src/ByteShortLong.java
+++ Hello/src/ByteShortLong.java
@@ -18,5 +18,23 @@ public class ByteShortLong {
         byte myMaxByteValue = Byte.MAX_VALUE;
         System.out.println("Byte Minimum Value = " + myMinByteValue);
         System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;//By default Java treats the numbers you type as an integer unless you add a letter on the end and forces Java to treat that number as a long.
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        long bigLongLiteralValue = 2_147_483_647_234L;//Treats the number as a long
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767; //Max is 32767 for a short but will change it into an int
+
     }
 }
