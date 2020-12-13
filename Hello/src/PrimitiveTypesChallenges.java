ff4b9ca082b80dbc9a682019ac3ae00084610991
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-13 16:08:36 -0600

    finished up the Primitive Types Challenges

--- Hello/src/PrimitiveTypesChallenges.java
+++ Hello/src/PrimitiveTypesChallenges.java
@@ -10,5 +10,8 @@ public class PrimitiveTypesChallenges {
 
         long myLong = 50000 + 10L * (myByte + myShort + myInt);
         System.out.println(myLong);
+
+        short shortTotal = (short) (1000 + 10 * (myByte + myShort + myInt));
+        System.out.println(shortTotal); //Integers are the best data type for using digits in Java
     }
 }
