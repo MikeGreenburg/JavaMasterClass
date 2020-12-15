e917778a1f21ed97330aacd82b17514cb762c5c4
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-14 22:22:38 -0600

    finished the float and double exercise

--- Hello/src/FloatAndDouble.java
+++ Hello/src/FloatAndDouble.java
@@ -11,5 +11,13 @@ public class FloatAndDouble {
         double myMaxDoubleValue = Double.MAX_VALUE;
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;// adding a f at the end is also allowed (5f) if using literal numbers by default a floating point number (5.25) is recognized as a double so it must be written as 5.25f to be a float or put (float) 5.25;
+        double myDoubleValue = 5;// adding a d at the end is also allowed (5d) if using literal numbers. Doubles are the preferred floating point number**
+
+
+
+
     }
 }
