b5ccee434e893cd97ae866f9d53810ceae0743a7
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-15 19:03:51 -0600

    doing the floating point precision challenge

--- Hello/src/FloatAndDouble.java
+++ Hello/src/FloatAndDouble.java
@@ -12,9 +12,12 @@ public class FloatAndDouble {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;// adding a f at the end is also allowed (5f) if using literal numbers by default a floating point number (5.25) is recognized as a double so it must be written as 5.25f to be a float or put (float) 5.25;
-        double myDoubleValue = 5;// adding a d at the end is also allowed (5d) if using literal numbers. Doubles are the preferred floating point number**
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;// adding a f at the end is also allowed (5f) if using literal numbers by default a floating point number (5.25) is recognized as a double so it must be written as 5.25f to be a float or put (float) 5.25 = casting;
+        double myDoubleValue = 5.00 / 3.00;// adding a d at the end is also allowed (5d) if using literal numbers. Doubles are the preferred floating point number**
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubelValue =" + myDoubleValue);
 
 
 
