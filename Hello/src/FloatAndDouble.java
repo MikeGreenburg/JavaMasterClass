0edd14799404c93f38a98cc8b5d78da98374a8ad
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-15 19:19:32 -0600

    finished the floating point challenge

--- Hello/src/FloatAndDouble.java
+++ Hello/src/FloatAndDouble.java
@@ -19,6 +19,16 @@ public class FloatAndDouble {
         System.out.println("MyFloatValue = " + myFloatValue);
         System.out.println("MyDoubelValue =" + myDoubleValue);
 
+        double myPounds = 1d;
+        double myKilograms = myPounds * 0.45359237d;
+        System.out.println("My weight in Kilograms are = " + myKilograms);
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
+
+
 
 
 
