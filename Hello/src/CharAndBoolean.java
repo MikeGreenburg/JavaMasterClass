ffe6aa3bcbaf3317cf90003c87e20dd2393df1f7
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-16 22:57:24 -0600

    doing the char and boolean exercise

---
+++ Hello/src/CharAndBoolean.java
@@ -0,0 +1,11 @@
+public class CharAndBoolean {
+
+    public static void main(String[] args) {
+
+        char myChar = 'M';
+        char myUnicodeChar = '\u004D';
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+    }
+}
