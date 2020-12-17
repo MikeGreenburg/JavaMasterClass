c0735ee6224eb866ffdfded7a93da97b7bcd0e41
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-16 23:01:37 -0600

    finished the char and boolean exercise

--- Hello/src/CharAndBoolean.java
+++ Hello/src/CharAndBoolean.java
@@ -7,5 +7,14 @@ public class CharAndBoolean {
 
         System.out.println(myChar);
         System.out.println(myUnicodeChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+        boolean isCustomerUnderTwentyOne = false;
     }
 }
