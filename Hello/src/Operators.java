eecd4a8e7673889c71f21a931af17f37ff4df78e
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-18 23:57:39 -0600

    ddoing the if then statement

--- Hello/src/Operators.java
+++ Hello/src/Operators.java
@@ -43,6 +43,10 @@ public class Operators {
         result -= 2; // 10 - 2 = 8
         System.out.println("10 - 2 = " + result);
 
-
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an Alien!");
+            System.out.println("And I am scared of Aliens!");
+        }
     }
 }
