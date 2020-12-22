c5f817359f12696334a44079bf12cbec16fd8cf5
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-21 23:58:39 -0600

    doing the assignment operator vs equals to operator exercise

--- Hello/src/Operators.java
+++ Hello/src/Operators.java
@@ -54,13 +54,18 @@ public class Operators {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 81;
+        int secondTopScore = 95;
         if ((topScore > secondTopScore) && (topScore < 100)) {
             System.out.println("Greater than second top score and less than 100");
         }
 
         if ((topScore > 90) || (secondTopScore <= 90)){
             System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is an error");
         }
 
     }
