2d17f4466101b1a8ef076bd68532170e18670ed6
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-25 23:29:09 -0600

    finished the expressions exercise

--- Hello/src/Expressions.java
+++ Hello/src/Expressions.java
@@ -11,5 +11,11 @@ public class Expressions {
         }
 
         //In the following code that I will type below, write down what parts of the code are expressions
+        int score = 100;//score = 100 is the expression
+
+        if (score < 99){ //expression is in parenthesis
+            System.out.println("You got the high score");//expression is in parenthesis
+//            score = 0;//score = 0 is an expression
+        }
     }
 }
