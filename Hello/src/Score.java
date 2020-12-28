d0c3bd6267dc85c2a3499538154bb13c7d015071
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-27 22:29:53 -0600

    doing the methods exercise

--- Hello/src/Score.java
+++ Hello/src/Score.java
@@ -1,24 +1,28 @@
-public class CodeBlocks {
+public class Score {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore(true, 800, levelCompleted, bonus);
+        calculateScore();
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        if(gameOver){
+        if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);
         }
-
     }
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+    public static void calculateScore() {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
