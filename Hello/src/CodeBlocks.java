19edf7a576ab44d9af6fcfa972093a55f3f9e08d
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-28 23:35:27 -0600

    doing the more methods exercise

--- Hello/src/CodeBlocks.java
+++ Hello/src/CodeBlocks.java
@@ -2,9 +2,20 @@ public class CodeBlocks {
 
     public static void main(String[] args){
 
-        calculateScore(true, 800, 5, 100);
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-        calculateScore(true, 10000, 8, 200);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
     }
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -12,7 +23,7 @@ public class CodeBlocks {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
+
             return finalScore;
         } else {
             return -1;
