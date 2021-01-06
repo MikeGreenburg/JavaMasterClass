0c29fcefcbf28c4de8370943a35e1b011e54ebe5
MichaelGreenburg <m.pgreenburg@gmail.com>
2021-01-05 23:13:19 -0600

    optimizing the challenge code even further

--- Hello/src/CodeBlocks.java
+++ Hello/src/CodeBlocks.java
@@ -63,13 +63,12 @@ public class CodeBlocks {
 
         if (playerScore >= 1000) {
             return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
+        } else if (playerScore >= 500) {
             return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
+        } else if (playerScore >= 100) {
             return 3;
-        } else {
-            return 4;
         }
+            return 4;
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
